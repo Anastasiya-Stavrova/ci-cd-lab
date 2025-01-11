a799160fe816bf9d012e0260da9ecd2afc6cd403
Anastasia <158266030+Anastasiya-Stavrova@users.noreply.github.com>
2025-01-11 16:02:05 +0700

    test

---
+++ .gitlab-ci/notify.sh
@@ -0,0 +1,6 @@
+printf '{"chat_id": "%s", "text": "%s", "parse_mode": "Markdown"}' \
+        "$TELEGRAM_CHAT_ID" $1 |
+      curl -s -X POST \
+        -H "Content-Type: application/json" \
+        -d @- \
+        "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
