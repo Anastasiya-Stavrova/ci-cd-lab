59d6a36dd37cfa3f52ab068c3bf96b383d898ca8
Anastasia <158266030+Anastasiya-Stavrova@users.noreply.github.com>
2025-01-11 21:02:09 +0700

    check rollback

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Pulling latest images for application..."
-docker compose pull
-
-echo "Container startup..."
-docker compose up -d --force-recreate
-
-echo "Application successfully deployed!"
